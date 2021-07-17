24fc9351255a8a7e519e4691eeff53353bf14df7
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2021-07-16 23:11:31 -0300

    add edit

--- controllers/views.py
+++ controllers/views.py
@@ -28,6 +28,30 @@ def creat():
     return redirect(url_for('index'))
 
 
+@app.route('/edit/<string:id_planet>')
+def edit(id_planet):
+    req = requests.get(f'http://127.0.0.1:5000/api/planet/{id_planet}')
+    planet = req.json()
+    print(planet)
+    print(type(planet))
+    planet = planet[0]  # list --> dictionary
+    return render_template('edit.html', titulo='Planet Update',
+                           planet=planet)
+
+
+@app.route('/update', methods=['POST', ])
+def update():
+    id_planet = request.form['id_planet']
+    name = request.form['name']
+    climate = request.form['climate']
+    terrain = request.form['terrain']
+    payload = {'name': name,
+               'climate': climate,
+               'terrain': terrain}
+    requests.put(f'http://127.0.0.1:5000/api/planet/{id_planet}', json=payload)
+    return redirect(url_for('index'))
+
+
 @app.route('/delete/<string:id_planet>')
 def delete(id_planet):
     requests.delete(f'http://127.0.0.1:5000/api/planet/{id_planet}')
